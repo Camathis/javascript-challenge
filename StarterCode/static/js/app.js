42c52a81d6c0d831ee19595c6cfe542ea5705464
Crystal A. Mathis <crystal.a.mathis@gmail.com>
2019-11-19 18:17:24 -0600

    Final HW commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,38 +8,30 @@ var output = d3.select("tbody");
 
 console.log(data);
 
-data.forEach(function(ufoSightings) {
-    console.log(ufoSightings);
+function populate(data)
+{
+  data.forEach(function(ufoSightings) {
     var row = tbody.append("tr");
     Object.entries(ufoSightings).forEach(function([key, value]) {
-      console.log(key, value);
- 
       var cell = row.append("td");
       cell.text(value);
     });
   });
+}
+
+populate(tableData);
 
 button.on("click", function() {
-
+  output.html("");
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
   console.log(inputValue);
-  console.log(tableData); 
+  // console.log(tableData); 
+
 
   var filteredData = tableData.filter(row => row.datetime === inputValue);
-  
-  filteredData = filteredData.forEach(function(filteredSightings) {
-    console.log(filteredSightings);
-    var row = output.append("tr");
-    Object.entries(filteredSightings).forEach(function([key, value]) {
-      console.log(key, value);
- 
-      var cell = row.append("td");
-      cell.text(value);
-      console.log(filteredData);
-    });
-  });
+  populate(filteredData);
 });
